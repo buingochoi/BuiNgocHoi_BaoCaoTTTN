25136b66fef081abd7bb596c0328b4a9cc29c06a
buingochoi <144342265+buingochoi@users.noreply.github.com>
2023-11-16 15:56:15 +0700

    Bui Ngoc Hoi
    Báo cáo đồ án

---
+++ doan_TTTN/database/migrations/2023_10_11_101724_create_banner_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('db_banner', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down():void
+    {
+        Schema::create('db_brand', function (Blueprint $table) {
+            $table->id(); //id
+            $table->string('name'); 
+            $table->string('image');
+            $table->string('link', 1000);
+            $table->string('position');
+            $table->string('description',1000);
+            $table->timestamps(); //created_at, updated_at
+            $table->unsignedInteger('created_by')->default(1);
+            $table->unsignedInteger('updated_by')->nullable();
+            $table->unsignedTinyInteger('status')->default(2);
+        });
+    }
+};
